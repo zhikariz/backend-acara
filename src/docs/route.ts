3dbe5342d928f1baa02d86f04703b50d2b5c398a
Helmi <helmi.prasetyo12@gmail.com>
2025-04-09 21:39:58 +0700

    Update route.ts

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,12 +1,21 @@
-import { Express } from "express"
-import swaggerUi from "swagger-ui-express"
-import swaggerOutput from "./swagger_output.json"
+import { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerOutput from "./swagger_output.json";
 import fs from "fs";
 import path from "path";
-
 export default function docs(app: Express) {
-  const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8")
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput, {
-    customCss: css
-  }))
+  const css = fs.readFileSync(
+    path.resolve(
+      __dirname,
+      "../../node_modules/swagger-ui-dist/swagger-ui.css"
+    ),
+    "utf-8"
+  );
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerOutput, {
+      customCss: css,
+    })
+  );
 }
